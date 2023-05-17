2887c4db7bb9e6c84f0acbf204a7651d3763cb70
David Diaz <3006692160101@ingenieria.usac.edu.gt>
2023-05-17 16:37:52 -0600

    [UPDATE]Fix validations
    Se implemento algoritmo que verifica que no hay IDS repetidos y manda un mensaje por consola.

--- API-Python/main.py
+++ API-Python/main.py
@@ -13,6 +13,20 @@ def getRandomJokes():
             data = response.json()
             if data['id'] not in [joke.get('id') for joke in jokes]:
                 jokes.append({'id':data['id'], 'value':data['value']})
+    #verificar que no hay IDS Repetidos
+    ids_repeat = []
+    ids = set()
+    for item in jokes:
+        if item['id'] in jokes:
+            ids_repeat.append(item['id'])
+        else:
+             ids.add(item['id'])
+
+    if ids_repeat:
+        print("Se encontraron ids repetidos")
+    else:
+        print("no existen IDS repetidos")
+
     return jsonify(jokes)
 
 if __name__ == '__main__':
